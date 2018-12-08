13a917bc7bdaf6acbf6c3a69e1b3b7d2041197a1
Jason Thompson <jasonthompson@MacBook-Air.hitronhub.home>
2018-12-08 11:00:45 -0800

    set up underscores with basic styles

---
+++ wp-content/themes/img210portfolio-child/functions.php
@@ -0,0 +1,23 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+   wp_enqueue_style( 'child-style',
+       get_stylesheet_directory_uri() . '/style.css',
+       array('parent-style')
+   );
+}
+
+// Add this to the functions.php file of your WordPress theme
+// It filters the mime types using the upload_mimes filter hook
+// Add as many keys/values to the $mimes Array as needed
+
+// function my_custom_upload_mimes($mimes = array()) {
+
+// 	// Add a key and value for the CSV file type
+// 	$mimes['svg'] = "image/svg+xml";
+
+// 	return $mimes;
+// }
+
+// add_action('upload_mimes', 'my_custom_upload_mimes');
